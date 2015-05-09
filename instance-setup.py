ee1eefa7dbdcefca4a55ebce7096c45f5ee0ce46
Brian Warner <warner@lothar.com>
2015-05-08 17:26:32 -0700

    don't run instance-setup twice, install tahoe if asked

--- instance-setup.py
+++ instance-setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
-# this is run by a GCE instance at boot, as a normal user. "git" and
-# python-requests are available, but not necessarily anything else.
+# this is run by a GCE instance at boot, as a normal user. "git" is
+# available, but not necessarily anything else.
 
 import sys
 from os.path import exists, expanduser
@@ -14,6 +14,28 @@ def log(s):
 
 def calls(s):
     return call(s.split())
+
+if exists("instance-setup.stamp"):
+    sys.exit(0)
+with open("instance-setup.stamp","w") as f:
+    f.write("run\n")
+
+calls("apt-get update")
+calls("apt-get dist-upgrade -y")
+calls("apt-get install -y python-requests")
+
+def get_metadata(name, type="instance"):
+    url = "http://metadata/computeMetadata/v1/%s/attributes/%s" % (type, name)
+    log("fetching %s" % url)
+    r = requests.get(url, headers={"Metadata-Flavor": "Google"})
+    if not r.ok and r.status_code == 404:
+        return None
+    r.raise_for_status()
+    return r.text
+
+if get_metadata("install-tahoe"):
+    print "installing tahoe (.dpkg)"
+    calls("apt-get install -y tahoe-lafs")
 
 TAHOE = expanduser("~/bin/tahoe")
 
@@ -32,13 +54,6 @@ else:
     log("~/bin/tahoe now ready")
 log("")
 sys.stdout.flush()
-
-def get_metadata(name, type="instance"):
-    url = "http://metadata/computeMetadata/v1/%s/attributes/%s" % (type, name)
-    log("fetching %s" % url)
-    r = requests.get(url, headers={"Metadata-Flavor": "Google"})
-    r.raise_for_status()
-    return r.text
 
 introducer_furl = get_metadata("introducer-furl", "project")
 
