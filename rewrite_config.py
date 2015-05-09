43a05abc3e39ab622811697db40207af70c9fc75
Brian Warner <warner@lothar.com>
2015-05-09 09:58:10 -0700

    ugh, I can't code before 10AM

--- rewrite_config.py
+++ rewrite_config.py
@@ -1,15 +1,15 @@
 import os
 
-def reconfig(fn, value, new):
+def reconfig(fn, key, newvalue):
     newfn = fn+".new"
     new = open(newfn, "w")
     found = False
     for line in open(fn,"r").readlines():
-        if line.startswith(value) or line.startswith("#"+value):
-            line = "%s = %s\n" % (value, new)
+        if line.startswith(key) or line.startswith("#"+key):
+            line = "%s = %s\n" % (key, newvalue)
             found = True
         new.write(line)
     new.close()
     if not found:
-        raise ValueError("did not find %s in %s" % (value, new))
+        raise ValueError("did not find %s in %s" % (key, fn))
     os.rename(newfn, fn)
