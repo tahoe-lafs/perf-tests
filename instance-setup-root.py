8a0849763df808b943d2dda0e40dc3567ab6ffef
Brian Warner <warner@lothar.com>
2015-05-08 21:12:04 -0700

    mount volumes better

--- instance-setup-root.py
+++ instance-setup-root.py
@@ -6,7 +6,6 @@
 import sys
 from os.path import exists, expanduser
 from subprocess import call
-import requests
 
 def log(s):
     print s
@@ -15,15 +14,19 @@ def log(s):
 def calls(s):
     return call(s.split())
 
+first_time = True
 if exists("instance-setup-root.stamp"):
-    log("instance-setup-root.stamp exists, exiting")
-    sys.exit(0)
+    log("instance-setup-root.stamp exists")
+    first_time = False
 with open("instance-setup-root.stamp","w") as f:
     f.write("run\n")
 
-calls("apt-get update")
-calls("apt-get dist-upgrade -y")
-calls("apt-get install -y python-requests")
+if first_time:
+    calls("apt-get update")
+    calls("apt-get dist-upgrade -y")
+    calls("apt-get install -y python-requests")
+
+import requests
 
 def get_metadata(name, type="instance"):
     url = "http://metadata/computeMetadata/v1/%s/attributes/%s" % (type, name)
@@ -34,17 +37,25 @@ def get_metadata(name, type="instance"):
     r.raise_for_status()
     return r.text
 
-if get_metadata("install-tahoe"):
+if first_time and get_metadata("install-tahoe"):
     print "installing tahoe (.dpkg)"
     calls("apt-get install -y tahoe-lafs")
 
-if "client" in get_metadata("tahoeperf-nodes"):
+if first_time and "client" in get_metadata("tahoeperf-nodes"):
     calls("apt-get install -y python-pip python-dev libssl-dev libffi-dev")
     calls("pip install -U pip")
     calls("pip install -U virtualenv gcloud")
 
+def mount(diskname, mountpoint):
+    log("mounting/formatting %s" % mountpoint)
+    if first_time:
+        calls("mkdir %s" % mountpoint)
+    call(["/usr/share/google/safe_format_and_mount", "-m", "mkfs.ext4 -F",
+          "/dev/disk/by-id/google-%s" % diskname, mountpoint])
+    calls("chown -R warner:warner %s" % mountpoint)
+
+for nodename in get_metadata("tahoeperf-nodes").split(","):
+    if nodename.startswith("storage"):
+        mount(nodename, expanduser("~warner/%s" % nodename))
+
 log("instance-setup-root.py complete")
-call(["/usr/share/google/safe_format_and_mount", "-m", "mkfs.ext4 -F", "/dev/disk/by-id/google-storage1", "/home/warner/storage1"])
-calls("chmod -R warner:warner /home/warner/storage1")
-call(["/usr/share/google/safe_format_and_mount", "-m", "mkfs.ext4 -F", "/dev/disk/by-id/google-storage2", "/home/warner/storage2"])
-calls("chmod -R warner:warner /home/warner/storage2")
