030331f68d2a872dbbb099f9fe4eba7f55fed4a0
Brian Warner <warner@lothar.com>
2015-05-10 11:35:04 -0700

    download.py: first version ready to test

--- download.py
+++ download.py
@@ -1,13 +1,22 @@
 #! /usr/bin/python
 
-import sys, httplib, json, random, time
-import pprint
+import os, sys, json, random, time, requests
+from gcloud import datastore
+from rewrite_config import restart_node, wait_for_connections
 
-#gateway = "http://localhost:3456/"
-#baseurl = gateway + "uri/"
-host = "localhost"
-port = 3456
-rootcap = "URI:DIR2-RO:7ouzgdjht6vofds7mwrxc3tuzq:tuqxb27hw774vfr3lpr26rpy5uwospeta5h6rzwqqgxmgtl2gvda"
+grid_config_id = sys.argv[1]
+trial_id = sys.argv[2]
+
+TAHOE = os.path.expanduser("~/bin/tahoe")
+BASEDIR = os.path.expanduser("~/.tahoe")
+EXPECTED_SERVERS = 6
+GATEWAY = "http://localhost:3456/"
+
+restart_node(TAHOE, BASEDIR)
+wait_for_connections(GATEWAY, EXPECTED_SERVERS)
+print "restarted"
+
+rootcap = "URI:DIR2:wnjcvliaektnjll7cqsly7fdvm:zdusnqxtf6iwvy2q5ps7irj7vplzueqcq2v2todred6b6v5r2pcq"
 M = 1000*1000
 
 SIZES = {M: "1MB",
@@ -18,39 +27,13 @@ SHARES = range(1, 60+1)
 def make_name(size, shares):
     return "%s--%d-of-%d" % (SIZES[size], shares, shares)
 
-# use just one connection for everything, might remove the TCP setup/shutdown
-# overhead. Also, might explode.
-connection = None
-
-def make_connection():
-    global connection
-    connection = httplib.HTTPConnection(host, port)
-    connection.connect()
-make_connection()
-
 def fetch(path):
-    connection.request("GET", path)
-    r = connection.getresponse()
-    if r.status < 200 or r.status >= 300:
-        print r.status, r.reaspon, r.read()
-        raise RuntimeError("HTTP failure")
-    return r.read()
+    url = GATEWAY + "uri/" + path
+    r = requests.get(url)
+    r.raise_for_status()
+    return r.text
 
-def discard_and_time(path):
-    start = time.time()
-    connection.request("GET", path)
-    r = connection.getresponse()
-    if r.status < 200 or r.status >= 300:
-        print r.status, r.reaspon, r.read()
-        raise RuntimeError("HTTP failure")
-    while True:
-        discard = r.read(64*1024)
-        if not discard:
-            break
-    elapsed = time.time() - start
-    return elapsed
-
-d = json.loads(fetch("/uri/"+rootcap+"?t=json"))
+d = json.loads(fetch(rootcap+"?t=json"))
 ntype, attrs = d
 childcaps = {} # key is (size, k)
 for size in SIZES:
@@ -61,23 +44,38 @@ for size in SIZES:
 
 #print childcaps
 
+key = datastore.Key("DownloadPerf")
+unpushed = []
+
 ITERATIONS = 10*M
-datafile = open("timing.out", "a")
 for i in range(ITERATIONS):
     size = random.choice(SIZES.keys())
     k = random.choice(SHARES)
+    N = k
     name = make_name(size, k)
     cap = childcaps[(size, k)]
-    if connection is None:
-        make_connection()
-    try:
-        download_time = discard_and_time("/uri/"+cap)
-        #print "total_download", size, SIZES[size], k, download_time
-        print >>datafile, "total_download", size, SIZES[size], k, download_time
-        datafile.flush()
-    except httplib.HTTPException, e:
-        print >>sys.stderr, time.ctime()
-        print >>sys.stderr, e
-        print >>sys.stderr, pprint.pformat(connection.__dict__)
-        connection = None
-        
+
+    start = time.time()
+    fetch(cap)
+    download_time = time.time() - start
+    c = datastore.Entity(key)
+    c.update({
+        "grid_config_id": grid_config_id,
+        "trial_id": trial_id,
+        "filetype": u"CHK",
+        "filesize": size,
+        "k": k,
+        "N": N,
+        #"max_segsize": "default", # use no-such-key to mean default
+        "filecap": cap.decode("ascii"),
+        "download_time": download_time,
+        })
+    unpushed.append(c)
+    if len(unpushed) > 5:
+        datastore.put(unpushed)
+        unpushed[:] = []
+    print "total_download", size, SIZES[size], k, download_time
+
+if unpushed:
+    datastore.put(unpushed)
+    unpushed[:] = []
