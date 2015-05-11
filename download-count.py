5fee71134068a4625cfe51388d75a3835d063bf2
Brian Warner <warner@lothar.com>
2015-05-10 18:22:21 -0700

    download-count.py: tool to count DownloadPerf records for a given trial

---
+++ download-count.py
@@ -0,0 +1,12 @@
+import sys
+from gcloud import datastore
+
+trial_id = int(sys.argv[1])
+
+q = datastore.Query(kind="DownloadPerf")
+q.add_filter("trial_id", "=", trial_id)
+count = len(q.fetch())
+print "%d records in DownloadPerf trial #%d" % (count, trial_id)
+dt = list(datastore.Query(kind="DownloadTrial", filters=[("trial_id","=",trial_id)]).fetch())[0]
+print "git_hash: %s" % dt["git_hash"]
+print "notes: %s" % dt["notes"]
