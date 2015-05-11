f8b417ebdcc510c9e7f4956d22da408cc9f29ed4
Brian Warner <warner@lothar.com>
2015-05-10 19:39:42 -0700

    app.yaml: get frontend app loading properly

--- frontend/api.py
+++ frontend/api.py
@@ -1,11 +1,10 @@
 import webapp2
 
-
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World! 2')
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/api', MainPage),
 ], debug=True)
