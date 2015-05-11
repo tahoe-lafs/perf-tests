e596ac695ff6e5a404df62a749728edd5c021aea
Brian Warner <warner@lothar.com>
2015-05-10 18:23:29 -0700

    oops

--- download-count.py
+++ download-count.py
@@ -5,7 +5,7 @@ trial_id = int(sys.argv[1])
 
 q = datastore.Query(kind="DownloadPerf")
 q.add_filter("trial_id", "=", trial_id)
-count = len(q.fetch())
+count = len(list(q.fetch()))
 print "%d records in DownloadPerf trial #%d" % (count, trial_id)
 dt = list(datastore.Query(kind="DownloadTrial", filters=[("trial_id","=",trial_id)]).fetch())[0]
 print "git_hash: %s" % dt["git_hash"]
