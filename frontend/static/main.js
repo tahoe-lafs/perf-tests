04e4fc629e5108350e9291b4b841f14de74b357f
Brian Warner <warner@lothar.com>
2015-05-13 15:24:03 -0700

    frontend: refactor series management, include trial 11

--- frontend/static/main.js
+++ frontend/static/main.js
@@ -35,21 +35,41 @@ var qs = (function(a) {
     return b;
 })(window.location.search.substr(1).split('&'));
 
+const MB = 1000*1000;
 var mode = qs["mode"] || "time";
 var trial = Number(qs["trial"] || 3);
-const partial_trials = [4];
+const partial_old_trials = [4];
+const partial_100MB_trials = [11];
+
+var series_functions = {
+    partial_old: function(p) {
+        var pushto;
+        if (p.k == 1) pushto = 0;
+        if (p.k == 3) pushto = 1;
+        if (p.k == 6) pushto = 2;
+        if (p.k == 30) pushto = 3;
+        if (p.k == 60) pushto = 4;
+        if (p.filesize == 10*MB) pushto += 5;
+        return pushto;
+    },
+    partial_100MB: function(p) {
+        if (p.k == 1) return 0;
+        if (p.k == 3) return 1;
+        if (p.k == 6) return 2;
+    },
+    by_filesize: function(p) {
+        if (p.filesize == 1*MB) return 0;
+        if (p.filesize == 10*MB) return 1;
+        if (p.filesize == 100*MB) return 2;
+    }
+};
 
 function reload() {
     console.log("loading trial="+trial+" mode="+mode);
     var url = "/api/downloads?trial_id=" + trial;
+    var seriesfunc;
     $.getJSON(url, function(data) {
-        if (partial_trials.indexOf(trial) == -1) {
-            chartOptions.xAxis.title.text = "k";
-            chartOptions.series = [{name: "1MB", data: []},
-                                   {name: "10MB", data: []},
-                                   {name: "100MB", data: []}
-                                  ];
-        } else {
+        if (partial_old_trials.indexOf(trial) != -1) {
             chartOptions.xAxis.title.text = "read size";
             chartOptions.series = [{name: "1MB k=1", data: []},
                                    {name: "1MB k=3", data: []},
@@ -62,6 +82,21 @@ function reload() {
                                    {name: "10MB k=30", data: []},
                                    {name: "10MB k=60", data: []}
                                   ];
+            seriesfunc = series_functions.partial_old;
+        } else if (partial_100MB_trials.indexOf(trial) != -1) {
+            chartOptions.xAxis.title.text = "read size";
+            chartOptions.series = [{name: "100MB k=1", data: []},
+                                   {name: "100MB k=3", data: []},
+                                   {name: "100MB k=6", data: []},
+                                  ];
+            seriesfunc = series_functions.partial_100MB;
+        } else {
+            chartOptions.xAxis.title.text = "k";
+            chartOptions.series = [{name: "1MB", data: []},
+                                   {name: "10MB", data: []},
+                                   {name: "100MB", data: []}
+                                  ];
+            seriesfunc = series_functions.by_filesize;
         }
         if (mode == "speed") {
             chartOptions.title = "Download Speed";
@@ -70,9 +105,8 @@ function reload() {
             chartOptions.title = "Download Time";
             chartOptions.yAxis.title.text = "seconds";
         }
-                          
+
         var s = chartOptions.series;
-        const MB = 1000*1000;
         data.perfs.forEach(function(p) {
             // .filesize, .k, .download_time
             var value;
@@ -80,32 +114,16 @@ function reload() {
                 value = p.filesize / p.download_time;
             else
                 value = p.download_time;
-            var x, pushto;
-            if (partial_trials.indexOf(trial) == -1) {
-                x = p.k;
-                if (p.filesize == 1*MB)
-                    pushto = 0;
-                if (p.filesize == 10*MB)
-                    pushto = 1;
-                if (p.filesize == 100*MB)
-                    pushto = 2;
-                s[pushto].data.push([x, value]);
-            } else {
+            var x;
+            if (partial_old_trials.indexOf(trial) != -1 ||
+                partial_100MB_trials.indexOf(trial) != -1) {
                 x = p.readsize;
-                if (p.k == 1)
-                    pushto = 0;
-                if (p.k == 3)
-                    pushto = 1;
-                if (p.k == 6)
-                    pushto = 2;
-                if (p.k == 30)
-                    pushto = 3;
-                if (p.k == 60)
-                    pushto = 4;
-                if (p.filesize == 10*MB)
-                    pushto += 5;
-                s[pushto].data.push([x, value]);
+                chartOptions.xAxis.min = 0;
+            } else {
+                x = p.k;
+                delete chartOptions.xAxis.min;
             }
+            s[seriesfunc(p)].data.push([x, value]);
         });
         var chart = new Highcharts.Chart(chartOptions);
     });
